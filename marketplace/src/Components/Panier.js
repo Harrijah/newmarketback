d5f21bd70bfbcce977e546a9d7d39aa2e712cc78
Hari <andrianarivohari@gmail.com>
2024-10-04 21:41:41 +0200

    hypocrisie

--- marketplace/src/Components/Panier.js
+++ marketplace/src/Components/Panier.js
@@ -11,7 +11,6 @@ const Panier = () => {
                     <li><b>Produit 02 </b> : <a href="#">Produit 02</a><button className="myfontawesome"> <i className="fa fa-trash-alt"></i></button></li>
                     <li><b>Produit 03 </b> : <a href="#">Produit 03</a><button className="myfontawesome"> <i className="fa fa-trash-alt"></i></button></li>
                     <li><b>Produit 04 </b> : <a href="#">Produit 04</a><button className="myfontawesome"> <i className="fa fa-trash-alt"></i></button></li>
-                    <li><b>Produit 05 </b> : <a href="#">Produit 05</a><button className="myfontawesome"> <i className="fa fa-trash-alt"></i></button></li>
                 </ul>
             </div>
 
